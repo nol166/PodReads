94ae28c9fdc609eccf7539412b9554922db3604b
John McCambridge <johnmccambridge@Johns-MacBook-Pro.local>
2018-01-19 17:15:13 -0600

    update config for knex and heroku

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/podReads'
-  }
-};
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+ }
